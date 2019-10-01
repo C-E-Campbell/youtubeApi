02f9e4be0d8bd9f9263ff3f3bccf7ccf3e3df31f
charles <codelyfewithcharlie@gmail.com>
2019-10-01 08:10:04 -0700

    Updates

---
+++ precache-manifest.071f9575763e41aeb4f61758b5e41a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdcee1ebec6ac745748aba7f35ed7c9",
+    "url": "/youtubeApi/index.html"
+  },
+  {
+    "revision": "b6f3e705f0a9694a138a",
+    "url": "/youtubeApi/static/css/main.6f088b76.chunk.css"
+  },
+  {
+    "revision": "88e81c510eda58d33db3",
+    "url": "/youtubeApi/static/js/2.e8f52b97.chunk.js"
+  },
+  {
+    "revision": "b6f3e705f0a9694a138a",
+    "url": "/youtubeApi/static/js/main.defd54c8.chunk.js"
+  },
+  {
+    "revision": "4a161c5ccbb6ac0365dc",
+    "url": "/youtubeApi/static/js/runtime-main.6adb0f56.js"
+  }
+]);
